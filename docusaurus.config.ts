ed55735533ff5f6e0b96804742921f8bf6a9cea2
J Nam <ngonam22@gmail.com>
2025-02-03 12:03:35 +0700

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,12 +13,13 @@ const config: Config = {
   url: 'https://ngonam22.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/hldv-doc',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ngonam22', // Usually your GitHub org/user name.
   projectName: 'hldv-doc', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
