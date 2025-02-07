0dc6fa43c3a9d1b46a78f4a91cb7b19dc086a985
J Nam <ngonam22@gmail.com>
2025-02-07 16:49:59 +0700

    update footer link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -126,8 +126,8 @@ const config: Config = {
           title: 'Docs',
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: 'Hướng Dẫn',
+              to: '/docs/loi-noi-dau',
             },
           ],
         },
